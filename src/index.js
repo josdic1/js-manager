ab50a946cdecdcc6546ced9a2b0f04349a92be50
josdic1 <joshuadicker@gmail.com>
2025-02-05 17:38:04 -0500

    added fetch, json, and dom elem vars

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const init = () => {
+
+  // DOM elements
+  const list = document.getElementById('list')
+
+  // stateful vars
+  let isLoading = true
+  let inEditMode = false
+  let genres = []
+  let formData = {
+    genre: '',
+    subgenres: []
+  }
+  let selectedGenre = {
+    id: '',
+    genre: '',
+    subgenres: []
+  }
+
+  // init fetch
+  fetchGenres()
+
+  // <----- LIST -------->
+
+
+
+  // <----- CRUD functions -------->
+  async function fetchGenres() {
+    try {
+      const r = await fetch(`http://localhost:3000/genres`)
+      if (!r.ok) {
+        throw new Error('GET: bad fetch')
+      }
+      const data = await r.json()
+      genres = data
+    } catch (error) { console.error(error) }
+  }
+
+}
+
+window.addEventListener("DOMContentLoaded", init)
+
+
+
